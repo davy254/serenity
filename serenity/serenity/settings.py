ca33dc38ebabdcad5e83a8ed27f4d2dc15efbee6
David <davidmunyiri2@gmail.com>
2020-08-20 15:08:46 +0300

    created profile page

--- serenity/serenity/settings.py
+++ serenity/serenity/settings.py
@@ -128,9 +128,11 @@ STATICFILES_DIRS = [
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL =  '/media/'
+MEDIA_URL = '/media/'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL='login'
